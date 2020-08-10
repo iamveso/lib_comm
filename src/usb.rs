af27c709b158d3b1e2ba7146fa5fe9c687d08a9a
Simon Akpoveso <simonakpoveso@Simons-MacBook-Pro.local>
2020-08-10 01:37:44 +0100

    created function to get In and Out endpoints for bulk transfer

--- src/usb.rs
+++ src/usb.rs
@@ -1,28 +1,20 @@
-use rusb::{Context, DeviceHandle, DeviceList, DeviceDescriptor};
+use rusb::{Context, DeviceHandle, DeviceList, DeviceDescriptor, TransferType, ConfigDescriptor, Interfaces, Interface, InterfaceDescriptors, InterfaceDescriptor, EndpointDescriptor, Direction, EndpointDescriptors};
 
-//Struct containing usb device identity
 struct UsbDeviceIdentity{
     vendor_id: u16,
     product_id: u16,
     handle: DeviceHandle<Context>,
-    endpoints: Endpoint,
+    endpoint: Endpoint,
 }
 
-//Struct for Endpoint
-struct Endpoint { dir_in: u8, dir_out: u8}
+struct Endpoint {dir_in: u8, dir_out: u8}
 
-//Find connected devices
 pub fn find_connected_devices() -> Result<DeviceHandle<Context>, rusb::Error>{
-    let context = create_device_context()?;
+    let context = Context::new()?;
     let list = DeviceList::new_with_context(context)?;
     get_device_handle(list)
 }
-//Create usb context
-pub fn create_device_context() -> Result<Context,rusb::Error>{
-    Context::new()
-}
 
-//Get device handle
 fn get_device_handle(list: DeviceList<Context>) -> Result<DeviceHandle<Context>, rusb::Error>{
     for device in list.iter(){
         let handle = device.open()?;
@@ -34,18 +26,49 @@ fn get_device_handle(list: DeviceList<Context>) -> Result<DeviceHandle<Context>,
     Err(rusb::Error::NotFound)
 }
 
-fn get_vendor_id(device: &DeviceHandle<Context>) -> Result<u16,rusb::Error>{
-    let desc = get_descriptor(device)?;
+fn get_vendor_id(device_handle: &DeviceHandle<Context>) -> Result<u16,rusb::Error>{
+    let desc = get_descriptor(device_handle)?;
     Ok(desc.vendor_id())
 }
 
-fn get_product_id(device: &DeviceHandle<Context>) -> Result<u16,rusb::Error>{
-    let desc = get_descriptor(device)?;
+fn get_product_id(device_handle: &DeviceHandle<Context>) -> Result<u16,rusb::Error>{
+    let desc = get_descriptor(device_handle)?;
     Ok(desc.product_id())
 }
 
-fn get_descriptor(device: &DeviceHandle<Context>) -> Result<DeviceDescriptor,rusb::Error> {
-    let device = device.device();
+fn get_descriptor(device_handle: &DeviceHandle<Context>) -> Result<DeviceDescriptor,rusb::Error> {
+    let device = device_handle.device();
     let descriptor = device.device_descriptor()?;
     Ok(descriptor)
+}
+
+fn get_bulk_endpoint_address(endpoint_descriptor: EndpointDescriptors) -> Result<Endpoint,rusb::Error>{
+    let mut e_in = Option::None;
+    let mut e_out = Option::None;
+    for endpoint in endpoint_descriptor{
+        if endpoint.transfer_type() != TransferType::Bulk {
+            continue;
+        }
+        if endpoint.direction() == Direction::In{
+            e_in = Some(endpoint.address());
+        }
+        if endpoint.direction() == Direction::Out {
+            e_out = Some(endpoint.address());
+        }
+    }
+
+    let e_in =  match e_in {
+        Some(s) => s,
+        None => return Err(rusb::Error::Other),
+    };
+
+    let e_out = match e_out {
+        Some(s) => s,
+        None => return Err(rusb::Error::Other),
+    };
+
+    return Ok(Endpoint{
+        dir_in: e_in,
+        dir_out: e_out,
+    })
 }
