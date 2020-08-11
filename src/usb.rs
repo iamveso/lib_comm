9e6ca1038093dcb2a9c3b44c58305a753e735d08
Simon Akpoveso <simonakpoveso@Simons-MacBook-Pro.local>
2020-08-11 01:06:28 +0100

    Usb Identity updated to contain handle, vendorid and productid

--- src/usb.rs
+++ src/usb.rs
@@ -1,13 +1,35 @@
 use rusb::{Context, DeviceHandle, DeviceList, DeviceDescriptor, TransferType, ConfigDescriptor, Interfaces, Interface, InterfaceDescriptors, InterfaceDescriptor, EndpointDescriptor, Direction, EndpointDescriptors, Device};
 
-struct UsbDeviceIdentity{
+pub struct UsbDeviceIdentity{
     vendor_id: u16,
     product_id: u16,
     handle: DeviceHandle<Context>,
-    endpoint: Endpoint,
+    //TODO:Endpoints are part of the devices identity
+}
+
+impl UsbDeviceIdentity{
+    pub fn print_struct(&self){
+        println!("Vendor ID is: {}", self.vendor_id);
+        println!("Product ID is: {}", self.product_id);
+        //TODO:Print out device endpoints
+    }
 }
 
 struct Endpoint {dir_in: u8, dir_out: u8}
+
+pub fn create_device_identity() -> Result<UsbDeviceIdentity,rusb::Error>{
+    let handle = find_connected_devices()?;
+    let vendor_id = get_vendor_id(&handle)?;
+    let product_id = get_product_id(&handle)?;
+    //let endpoints = get_endpoints(&handle)?;
+    let device_identity = UsbDeviceIdentity{
+        vendor_id,
+        product_id,
+        handle,
+        //endpoint: endpoints,
+    };
+    Ok(device_identity)
+}
 
 ///Returns a Result<> of device handle on success
 pub fn find_connected_devices() -> Result<DeviceHandle<Context>, rusb::Error>{
@@ -20,8 +42,7 @@ fn get_device_handle(list: DeviceList<Context>) -> Result<DeviceHandle<Context>,
     for device in list.iter(){
         let handle = device.open()?;
         //TODO:Get interface number dynamically
-        let interace_number = get_interface_number(&device)?;
-        let attached = handle.kernel_driver_active(interace_number)?;
+        let attached = handle.kernel_driver_active(0)?;
         if !attached{
             return Ok(handle)
         }
@@ -37,85 +58,4 @@ fn get_vendor_id(device_handle: &DeviceHandle<Context>) -> Result<u16,rusb::Erro
 fn get_product_id(device_handle: &DeviceHandle<Context>) -> Result<u16,rusb::Error>{
     let desc = get_descriptor(device_handle)?;
     Ok(desc.product_id())
-}
-
-fn get_descriptor(device_handle: &DeviceHandle<Context>) -> Result<DeviceDescriptor,rusb::Error> {
-    let device = device_handle.device();
-    let descriptor = device.device_descriptor()?;
-    Ok(descriptor)
-}
-
-fn get_interface_number(device: &Device<Context>) -> Result<u8, rusb::Error>{
-    let config_descriptor = get_config_descriptor(device)?;
-    let interfaces = get_interfaces(&config_descriptor);
-    for i in interfaces{
-        let desc = get_interface_descriptor(i.descriptors())?;
-        let output = desc.interface_number();
-        return Ok(output);
-    }
-    Err(rusb::Error::Other)
-}
-
-fn get_config_descriptor(device: &Device<Context>) -> Result<ConfigDescriptor,rusb::Error>{
-    device.active_config_descriptor()
-}
-
-fn get_interfaces(configuration: &ConfigDescriptor) -> Interfaces {
-    configuration.interfaces()
-}
-
-fn get_interface_descriptor(interface_descriptors: InterfaceDescriptors) -> Result<InterfaceDescriptor,rusb::Error>{
-    for i in interface_descriptors{
-        let e_descriptor = i.endpoint_descriptors();
-        let is_bulk = get_bulk_endpoint_address(e_descriptor);
-        match is_bulk {
-            Ok(s) => {
-                return Ok(i);
-            },
-            Err(_) => continue
-        }
-    }
-    Err(rusb::Error::BadDescriptor)
-}
-
-fn get_bulk_endpoint_address(endpoint_descriptor: EndpointDescriptors) -> Result<Endpoint,rusb::Error>{
-    let mut e_in = Option::None;
-    let mut e_out = Option::None;
-    for endpoint in endpoint_descriptor{
-        if is_bulk_transfer_type(&endpoint) {
-            continue;
-        }
-        if endpoint.direction() == Direction::In{
-            e_in = Some(endpoint.address());
-        }
-        if endpoint.direction() == Direction::Out {
-            e_out = Some(endpoint.address());
-        }
-    }
-
-    let e_in = get_endpoint_address(e_in)?;
-    let e_out = get_endpoint_address(e_out)?;
-
-    return Ok(add_endpoints(e_in,e_out))
-}
-
-fn is_bulk_transfer_type(endpoint: &EndpointDescriptor) -> bool{
-    if endpoint.transfer_type() == TransferType::Bulk{
-        return true;
-    }
-    false
-}
-
-fn get_endpoint_address(endpoint: Option<u8>) -> Result<u8,rusb::Error>{
-    return match endpoint{
-        Some(s) => Ok(s),
-        None => Err(rusb::Error::Other),
-    }
-}
-
-fn add_endpoints(endpoint_in: u8, endpoint_out: u8) -> Endpoint{
-    Endpoint{
-        dir_in: endpoint_in,
-        dir_out: endpoint_out,
-    }
 }
